4497991709abcdd447757ba2b6ab65292591f744
Vitor Santos <vitor.santos@poppinapp.com.br>
2017-04-29 13:19:32 -0300

    First client and server sketch

--- client.c
+++ client.c
@@ -1,1 +1,75 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
 
+#define SERVER_PORT 12345
+#define MAX_LINE 256
+
+int main(int argc, char *argv[])
+{
+  struct hostent *host_address;
+  struct sockaddr_in server;
+  char *host;
+  char buf[MAX_LINE];
+  int s;
+  int len;
+
+  /* verificação de argumentos */
+
+  // TODO
+
+  /* tradução de nome para endereço IP */
+
+  // TODO
+
+  /* criação da estrutura de dados de endereço */
+  //bzero((char *)&socket_address, sizeof(socket_address));
+
+  /* criação de socket ativo*/
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s == -1)
+  {
+    printf("Socket nao foi criado");
+  }
+  puts("Socket criado");
+
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_family = AF_INET;
+  server.sin_port = htons(SERVER_PORT);
+
+  /* estabelecimento da conexão */
+  if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+  {
+    perror("Falha no estabelecimento da conexao");
+    return 1;
+  }
+  puts("Conectado\n");
+
+  /* ler e enviar linhas de texto, receber eco */
+  while (1)
+  {
+    printf("Digite uma mensagem para ser enviada ao servidor: ");
+    scanf("%s", buf);
+
+    //Envia a mensagem
+    if (send(s, buf, strlen(buf), 0) < 0)
+    {
+      puts("Envio falhou");
+      return 1;
+    }
+
+    //Recebe a resposta do servidor
+    if (recv(s, buf, MAX_LINE, 0) < 0)
+    {
+      puts("Recebimento falhou");
+      break;
+    }
+    puts("Resposta do servidor: ");
+    puts(buf);
+  }
+
+  close(s);
+  return 0;
+}
--- server.c
+++ server.c
@@ -1,1 +1,86 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
 
+#define LISTEN_PORT 12345
+#define MAX_PENDING 5
+#define MAX_LINE 256
+
+int main()
+{
+  struct sockaddr_in server, client;
+  char buf[MAX_LINE];
+  unsigned int len;
+  int s, new_s, c, read_size;
+
+  /* criação da estrutura de dados de endereço */
+  //bzero((char *)&server, sizeof(server));
+
+  /* criação de socket passivo */
+  // Argumentos 1) Internet domain 2) Stream socket 3) Default protocol (TCP)
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s == -1)
+  {
+    printf("Socket nao foi criado");
+  }
+  puts("Socket criado");
+
+  /* Associar socket ao descritor */
+  // Address family = Internet
+  server.sin_family = AF_INET;
+  // Porta na conexão
+  server.sin_port = htons(LISTEN_PORT);
+  // Ip da conexão
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  // Bind da conexão com o socket
+  if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+  {
+    perror("Bind falhou");
+    return 1;
+  }
+  puts("Bind completo");
+
+  /* Criar escuta do socket para aceitar conexões */
+  // Inicia conexão do socket com no máximo 5 conexoes simultaneas
+  listen(s, MAX_PENDING);
+
+  /* aguardar/aceita conexão, receber e imprimir texto na tela, enviar eco */
+  puts("Aguardarndo conexoes...");
+  c = sizeof(struct sockaddr_in);
+
+  //aceita conexoes de um client
+  while ((new_s = accept(s, (struct sockaddr *)&client, (socklen_t *)&c)))
+  {
+    puts("Conexao aceita");
+
+    //Recebe uma mensagem do client
+    while ((read_size = recv(new_s, buf, MAX_LINE, 0)) > 0)
+    {
+      puts("Mensagem recebida: ");
+      puts(buf);
+
+      //Send the message back to client
+      write(new_s, buf, strlen(buf));
+    }
+
+    if (read_size == 0)
+    {
+      puts("Cliente desconectado");
+      fflush(stdout);
+    }
+    else if (read_size == -1)
+    {
+      perror("Recebimento falhou");
+    }
+  }
+
+  if (new_s < 0)
+  {
+    perror("Aceite falhou");
+    return 1;
+  }
+
+  return 0;
+}
